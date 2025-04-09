6f5365788d3e864e55074ab92c92e4fb574c0681
iinigmatullin <iinigmatullin@diasoft.ru>
2025-04-09 22:27:31 +0500

    написать слой контроллеров

---
+++ src/main/java/org/example/controller/ImageController.java
@@ -0,0 +1,27 @@
+package org.example.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.service.ImageService;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/images")
+public class ImageController {
+
+    private final ImageService imageService;
+
+    @GetMapping(path = "/{id}", produces = MediaType.IMAGE_PNG_VALUE)
+    public ResponseEntity<Resource> image(@PathVariable("id") Long id) {
+        Resource path = imageService.getPath(id);
+        return ResponseEntity.ok()
+                .contentType(MediaType.IMAGE_PNG)
+                .body(path);
+    }
+}
---
+++ src/main/java/org/example/controller/PostController.java
@@ -0,0 +1,50 @@
+package org.example.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.domain.Post;
+import org.example.service.PostService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/posts")
+public class PostController {
+    private final PostService postService;
+
+    @GetMapping
+    public String posts(
+            @RequestParam(name = "pageNumber", defaultValue = "0") int page,
+            @RequestParam(name = "pageSize", defaultValue = "5") int pageSize,
+            Model model
+    ) {
+        Pageable pageable = PageRequest.of(page, pageSize);
+        Page<Post> posts = postService.findAll(pageable);
+        model.addAttribute("posts", posts.getContent());
+        model.addAttribute("paging", posts);
+        return "posts";
+    }
+
+    @GetMapping("/add")
+    public String add() {
+        return "add-post";
+    }
+
+    @PostMapping
+    public void addPost(
+            @RequestParam String title,
+            @RequestParam String text,
+            @RequestParam String tags,
+            @RequestParam MultipartFile image
+    ) {
+        postService.save(title, text, tags, image);
+    }
+}
