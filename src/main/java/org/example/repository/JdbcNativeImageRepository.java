c7dac0cca2bdd2de8f8dea2c285627b2c8fbb56b
iinigmatullin <iinigmatullin@diasoft.ru>
2025-04-12 13:41:56 +0500

    Изменить логику добавления image в репозиторном слое

--- src/main/java/org/example/repository/JdbcNativeImageRepository.java
+++ src/main/java/org/example/repository/JdbcNativeImageRepository.java
@@ -5,9 +5,14 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 @Repository
 @RequiredArgsConstructor
-public class JdbcNativeImageRepository implements ImageRepository{
+public class JdbcNativeImageRepository implements ImageRepository {
 
     private final JdbcTemplate jdbcTemplate;
 
@@ -23,11 +28,28 @@ public class JdbcNativeImageRepository implements ImageRepository{
 
     @Override
     public void save(MultipartFile image, Long postId) {
-        String query = """
-                INSERT INTO blog.images(path, post_id)
-                VALUES (?, ?)
-                """;
 
-        jdbcTemplate.update(query, image, postId)
+        if (!image.isEmpty()) {
+            String fileName = image.getOriginalFilename();
+            String tomcatDir = System.getProperty("catalina.base");
+            Path uploadDir = Paths.get(tomcatDir, "uploads", "images");
+            Path filePath = uploadDir.resolve(fileName);
+
+            try {
+                if (!Files.exists(uploadDir)) {
+                    Files.createDirectories(uploadDir);
+                }
+                Files.write(filePath, image.getBytes());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            final String QUERY = """
+                    INSERT INTO blog.images(path, post_id)
+                    VALUES (?, ?)
+                    """;
+            String relativePath = "/uploads/images/" + fileName;
+            jdbcTemplate.update(QUERY, relativePath, postId);
+        }
     }
 }
