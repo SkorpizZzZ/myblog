2bf89501589a5aed0ef8933b0dace804107e7d1e
iinigmatullin <iinigmatullin@diasoft.ru>
2025-04-12 13:44:16 +0500

    Изменить возвращаемый тип в методе получения image в ImageService.java

--- src/main/java/org/example/service/ImageService.java
+++ src/main/java/org/example/service/ImageService.java
@@ -2,20 +2,21 @@ package org.example.service;
 
 import lombok.RequiredArgsConstructor;
 import org.example.repository.ImageRepository;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 
 @Service
 @RequiredArgsConstructor
 public class ImageService {
     private final ImageRepository imageRepository;
-    private final ResourceLoader resourceLoader;
 
 
-    public Resource getPath(Long id) {
+    public File getImage(Long id) throws FileNotFoundException {
         String relativePath = imageRepository.findPathByPostId(id);
-        return new ClassPathResource(relativePath);
+        String catalinaBase = System.getProperty("catalina.base");
+        return ResourceUtils.getFile(catalinaBase.concat(relativePath));
     }
 }
