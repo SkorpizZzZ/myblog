d6e36cd7950ae95c8657eb4055547478d391e3ef
iinigmatullin <iinigmatullin@diasoft.ru>
2025-04-12 13:37:36 +0500

    добавить bean MultipartResolver

--- src/main/java/org/example/configuration/WebConfiguration.java
+++ src/main/java/org/example/configuration/WebConfiguration.java
@@ -1,8 +1,11 @@
 package org.example.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebMvc
@@ -10,4 +13,8 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @PropertySource("classpath:application.properties")
 @Configuration
 public class WebConfiguration {
+    @Bean
+    public MultipartResolver multipartResolver() {
+        return new StandardServletMultipartResolver();
+    }
 }
