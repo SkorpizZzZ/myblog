78aa1e2514d921437e9e2a4fc12f1970624d5060
iinigmatullin <iinigmatullin@diasoft.ru>
2025-05-04 20:44:10 +0500

    Убрать конфиг классы, перенести ивент создания структуры бд в отдельный Листнер

--- src/main/java/org/example/myblogspringboot/listener/DatabasePopulatorListener.java
+++ src/main/java/org/example/myblogspringboot/listener/DatabasePopulatorListener.java
@@ -1,19 +1,15 @@
-package org.example.myblogspringboot.configuration;
+package org.example.myblogspringboot.listener;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 
-
-@Configuration
-@EnableTransactionManagement
-public class DataSourceConfiguration {
-
+@Component
+public class DatabasePopulatorListener {
     @EventListener
     public void populate(ContextRefreshedEvent event) {
         DataSource dataSource = event.getApplicationContext().getBean(DataSource.class);
